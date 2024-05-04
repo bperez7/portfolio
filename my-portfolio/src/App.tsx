5cf2ed53fe81664312de0c1eb2ab08fad45d6df0
Brandon Perez <tohokubp@gmail.com>
2024-05-04 10:10:31 -0400

    routes

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import './styles.css';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 const App: React.FC = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
-      <Route path="" element={<Home />} />
+      <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
